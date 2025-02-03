8d1dff0535fe6ee954025417c32a38c3d69ee2e4
Kevin Chen <kchen@lcs.on.ca>
2025-02-03 15:10:56 -0500

    Initial Commit

---
+++ GroceryStoreFlyer/GroceryStoreFlyerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryStoreFlyerApp.swift
+//  GroceryStoreFlyer
+//
+//  Created by Haowen Chen on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct GroceryStoreFlyerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
